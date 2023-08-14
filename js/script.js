4dc50f2abe583cbcec4a131c0e2cc645f38e682a
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2023-08-14 02:28:32 -0300

    Fix js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 document.querySelector(".button-privacy").addEventListener("click", function() {
-    // Change background to dancing rick
-    document.body.style.backgroundImage = "url('images/download.gif')"
+    document.body.style.backgroundImage = "url('images/download.gif')";
+    document.querySelector(".container").style.display = "none";
+    const song = document.getElementsByTagName("audio")[1];
+    song.play();
+});
 
-    // Hide the heading and the button
-    document.querySelector(".container").style.display = "none"
-
-    // Play song
-    var song = document.getElementsByTagName("audio")[1]
-    song.play()
-})
+document.addEventListener("keyup", ev => {
+    if (ev.key === "Escape") {
+        window.location.reload();
+    }
+});
